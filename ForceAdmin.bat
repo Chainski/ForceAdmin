537d922285ee00a378f472a2b86ac5a29af8f08f
CHA1NSK1 <96607632+Chainski@users.noreply.github.com>
2022-12-20 21:16:50 -0500

    Add files via upload

---
+++ ForceAdmin.bat
@@ -0,0 +1,10 @@
+<# :batch script
+:: https://github.com/Chainski/ForceAdmin
+@echo off
+setlocal
+cd %~dp0
+powershell -ep remotesigned -Command "IEX $([System.IO.File]::ReadAllText('%~f0'))"
+endlocal
+goto:eof
+#>
+while($true){try{Start-Process 'cmd' -Verb runas -ArgumentList 'cmd /k whoami /priv';exit}catch{}}
